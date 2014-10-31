9738ff97599a6c0bd6cb1ae92243f982a298673d
kimberlymartin <kimberlymartin99@gmail.com>
2014-10-31 14:38:44 -0500

    100pt

--- 100pt.py
+++ 100pt.py
@@ -27,14 +27,15 @@ class MyApp:
 		self.button1.grid(row=0,column=0)
 		
 	        # Add a second button!
-				
-		
+		self.button2 = Button(self.myContainer1)
+		self.button1.configure(text="Right", background= "green")
+		self.button2.grid(row=0,column=1)
 						
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
 		# Create the code to bind an action to the second button
 		# Do not change "<Button-1>"
-		 
+		self.button2.bind("<Button-1>", self.button2Click)
 		  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
@@ -46,9 +47,13 @@ class MyApp:
                 # "global" makes sure that we can access our oval and our drawpad
 		global oval
 		global drawpad
+		drawpad.move(oval,-5,0)
 	
 	# Add the event handler for the second button to make it move right!
-	
+	def button2Click(self,event):
+	        global oval
+	        global drawpad
+		drawpad.move(oval,5,0)
 		
 myapp = MyApp(root)
 root.mainloop()
